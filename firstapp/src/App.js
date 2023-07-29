5c9ddb179791573ebc5b4f86f2ead7e733952169
Alisher <alisherxujanov163@gmail.com>
2023-07-29 09:53:16 +0500

    useState()  - lesson

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,8 +1,21 @@
 import Heading from './Components/Heading.jsx'
+import Nav from './Components/Nav'
+import { useState } from 'react'
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  function changeTheme() {
+    setDarkTheme(!darkTheme)
+  }
+
   return (
-    <div className="App">
+    <div class={darkTheme ? 'dark-theme' : ''} className="App">
+      <button onClick={changeTheme}>
+        Change Theme
+      </button>
+
+      <Nav />
       <Heading>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt, nobis sequi. Modi doloribus maiores voluptates minus, facilis autem tempore? Repudiandae quae ratione natus incidunt, vitae ipsum tenetur esse id eaque!</p>
       </Heading>
---
+++ firstapp/src/Components/Nav/index.jsx
@@ -0,0 +1,17 @@
+import nav from './nav.scss';
+import { useState } from 'react';
+
+function Navbar() {
+    
+    return (
+        <nav style={nav}>
+            <h3>Test</h3>
+
+            <div >
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident accusantium suscipit error odit ipsam dolorum placeat et. Libero, debitis, aliquam quos possimus consectetur delectus blanditiis reiciendis earum, obcaecati iure eaque.</p>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
