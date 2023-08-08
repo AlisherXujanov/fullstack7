a7d6c96fb54ae8a769581a9d4632aa47418503df
Alisher <alisherxujanov163@gmail.com>
2023-08-08 09:52:20 +0500

    bootstrap components

--- firstapp/src/Components/MainPage.jsx
+++ firstapp/src/Components/MainPage.jsx
@@ -1,4 +1,10 @@
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Accordion from 'react-bootstrap/Accordion';
+import Pagination from 'react-bootstrap/Pagination';
+import Spinner from 'react-bootstrap/Spinner'
 
 
 function MainPage() {
@@ -7,6 +13,92 @@ function MainPage() {
             <h1>Main Page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolore, laboriosam earum quibusdam ratione reprehenderit nobis numquam dolorem similique repellendus natus. Nobis omnis deserunt quos excepturi eveniet natus animi obcaecati.</p>
             <Button variant="primary">Primary</Button>
+            <Button variant="success">Primary</Button>
+            <Button variant="outline-danger">Primary</Button>
+
+            {/* 
+                <table>
+                    <tr>
+                        <th></th>
+                        <td></td>
+                    </tr>
+                </table> 
+            */}
+
+            <Container>
+                <Row>
+                    <Col className="bg-info rounded p-2" sm={8}>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                    culpa qui officia deserunt mollit anim id est laborum.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </Col>
+                    <Col className="bg-success rounded text-light p-2" xs={4}>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus explicabo iste nulla recusandae exercitationem, dolorum quam excepturi cum necessitatibus debitis quidem perferendis consectetur? Autem dicta necessitatibus voluptatibus nam porro perferendis.</p>
+                        <Button variant="primary" disabled>
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            <span className="visually-hidden">Loading...</span>
+                        </Button>{' '}
+                        <Button variant="primary" disabled>
+                            <Spinner
+                                as="span"
+                                animation="grow"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            Loading...
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="container">
+                <Pagination>
+                    <Pagination.First />
+                    <Pagination.Prev />
+                    <Pagination.Item>{1}</Pagination.Item>
+                    <Pagination.Ellipsis />
+
+                    <Pagination.Item>{10}</Pagination.Item>
+                    <Pagination.Item>{11}</Pagination.Item>
+                    <Pagination.Item active>{12}</Pagination.Item>
+                    <Pagination.Item>{13}</Pagination.Item>
+                    <Pagination.Item disabled>{14}</Pagination.Item>
+
+                    <Pagination.Ellipsis />
+                    <Pagination.Item>{20}</Pagination.Item>
+                    <Pagination.Next />
+                    <Pagination.Last />
+                </Pagination>
+            </div>
         </>
     );
 }
