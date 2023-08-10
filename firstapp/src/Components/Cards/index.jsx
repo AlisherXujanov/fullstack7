fc19bb94fe0509eb1be43c0299ee8c5192e9c81b
Alisher <alisherxujanov163@gmail.com>
2023-08-10 09:26:26 +0500

    Cards component basic

--- firstapp/src/Components/Cards/index.jsx
+++ firstapp/src/Components/Cards/index.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import cardStyles from "./card.scss";
+
 function Card() {
+    const [cards, setCards] = useState([]);
+
+    function createCard(event) {
+        event.preventDefault()
+        const newCard = {
+            id: Date.now(),
+            content: 'New Card'
+        }
+        setCards([...cards, newCard]);
+    }
+
     return (
-        <div>
+        <div style={cardStyles}>
             <h1>Cards</h1>
+
+            <form id="create-card-form"
+                onSubmit={e => { createCard(e) }}
+            >
+                <div className="form-control">
+                    <label htmlFor="card-title">Card Title</label>
+                    <input id="card-title" type="text" />
+                </div>
+
+                <button>Submit</button>
+            </form>
+
+
+            <hr />
+
+            <div className="cards">
+                {cards.map(card => {
+                    return (
+                        <div 
+                            key={card.id} 
+                            id={card.id + "-card"} 
+                            className="card"
+                        >
+                            <h3>{card.content}</h3>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
