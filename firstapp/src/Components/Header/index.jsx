2c49a62b25705ccdb0909a20da6146a5cd070dec
Alisher <alisherxujanov163@gmail.com>
2023-08-17 08:45:37 +0500

    Footer

--- firstapp/src/Components/Header/index.jsx
+++ firstapp/src/Components/Header/index.jsx
@@ -13,7 +13,31 @@ function Navbar() {
             </div>
 
             <footer>
-                <p>Footer</p>
+                <div className='left'>
+                    <img src="" alt="" width={50} height={50} />
+                    ©️ 2023 by Alisher & Fullstack Team
+                </div>
+                <div className="right">
+                    <ul>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Github</a></li>
+                        <li><a href="#">Pricing</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">API</a></li>
+                        <li><a href="#">Training</a></li>
+                        <li><a href="#">Status</a></li>
+                        <li><a href="#">Security</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Teams</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Docs</a></li>
+                    </ul>
+                </div>
             </footer>
         </>
     );
