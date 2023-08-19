8f7f16dee487b1c37a06d799defe8a99b2bb19cf
Alisher <alisherxujanov163@gmail.com>
2023-08-19 09:50:19 +0500

    onscroll

--- firstapp/src/Components/Header/Nav.jsx
+++ firstapp/src/Components/Header/Nav.jsx
@@ -3,12 +3,23 @@ import Theme from '../Theme'
 import audioFile from '../../Assets/click.mp3'
 import clickSound from '../../Assets/link_click.mp3'
 import logo from '../../Assets/images/logo.png'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 function Nav() {
     const [theme, setTheme] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            const scrollHeihgt = window.scrollY
+            if (scrollHeihgt < 10) {
+                document.querySelector('nav').style.backgroundColor = 'transparent'
+            } else {
+                document.querySelector('nav').style.backgroundColor = '#1f1f1f'
+            }
+        })
+    }, [window.scrollY])
 
     function playClickSound() {
         const sound = new Audio(clickSound);
