8e0feb17c17fa5ccbf33df6d096089e80f0a73f2
Alisher <alisherxujanov163@gmail.com>
2023-08-24 08:58:44 +0500

    reducer

--- firstapp/src/Components/MainPage.jsx
+++ firstapp/src/Components/MainPage.jsx
@@ -1,28 +1,34 @@
 import Button from 'react-bootstrap/Button';
 import MemoTest from './MemoTest';
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import { motion } from 'framer-motion'
 
-
+const initialState = { count: 0 }
+function reducer(state, action) {
+    // {type: "increment"}
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+    }
+}
 
 function MainPage() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
 
     const initialMotion = { transform: 'translateY(-100vw)' }
     const animate = { transform: 'translateY(0)' }
     const exit = { transform: 'translateY(100vw)' }
 
     return (
-        <motion.div
-            initial={initialMotion}
-            animate={animate}
-            exit={exit}
-        >
+        <motion.div initial={initialMotion} animate={animate} exit={exit} >
             <h1>Main Page</h1>
-            <h1>Count: {count}</h1>
-            <Button 
-                variant='outline-primary' 
-                onClick={() => { setCount(count + 1) }}
+            <h1>Count: {state.count}</h1>
+            <Button
+                variant='primary'
+                onClick={() => { dispatch({type: "increment"}) }}
             >
                 Increment
             </Button>
