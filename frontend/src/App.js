7f6bbdc54c48be74d144a4c98d1001504059fd05
Alisher <alisherxujanov163@gmail.com>
2023-11-30 09:53:47 +0500

    navbar started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
+import Navigation from "./components/Navigation"
+
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Navigation />
+      </nav>
+
       <h1>Hello world</h1>
     </div>
   );
---
+++ frontend/src/components/Navigation/index.jsx
@@ -0,0 +1,11 @@
+import './style.scss'
+
+function Navigation() {
+    return (
+        <div className="nav-wrapper">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quos voluptates sint corrupti ex nemo totam odio sunt aliquam fuga accusantium neque quidem minus veritatis ullam, error dicta cum repellat.</p>
+        </div>
+    );
+}
+
+export default Navigation;
