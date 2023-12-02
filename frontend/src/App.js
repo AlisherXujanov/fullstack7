98c6eeaa19949e763feeb8696bcc6012e834ac8a
Alisher <alisherxujanov163@gmail.com>
2023-12-02 10:01:27 +0500

    last update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,18 @@ import Navigation from "./components/Navigation"
 import About from "./components/About"
 
 function App() {
+  // const x = 1+1 == 2  ? 'malenkiy' : 'NE malenkiy'
+  // let a = {
+  //     x: 1,
+  //     y: {
+  //       z: 4
+  //     }
+  // }
+  // a.y.z 
+  // a.y?.z
+  // ... || ...
+  // [] && ...
+
   return (
     <div className="App">
       <nav>
@@ -9,7 +21,9 @@ function App() {
       </nav>
 
       <main>
-        <About />
+        <About title={"About page ..."} >
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, temporibus, recusandae veniam, doloribus minima officiis debitis nihil error est illum magni molestias vitae facilis laudantium. Incidunt saepe tenetur aliquam ullam.</p>
+        </About>
       </main>
     </div>
   );
--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,9 +1,22 @@
 import "./style.scss"
 
-function About() {
+
+// PROPS   =>  are the items that are passed through 
+//         the component when it is called 
+//         (as parameters in simple functions)
+// RU:     =>  это элементы, которые передаются через
+//         компонент при его вызове
+//         (как параметры в простые функции)
+// 
+
+
+function About({ title, children }) {
     return (
         <div className="about-page">
-            <h1>About page</h1>
+            <h1>{ title }</h1>
+            <div>
+                { children }
+            </div>
         </div>
     );
 }
