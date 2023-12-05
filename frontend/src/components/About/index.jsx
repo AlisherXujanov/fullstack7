8b52dab857b03244112ad49007cd1e909084df0d
Alisher <alisherxujanov163@gmail.com>
2023-12-05 08:52:43 +0500

    useState

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,18 +1,18 @@
 import "./style.scss"
+import { useState } from "react"
 
-
-// PROPS   =>  are the items that are passed through 
-//         the component when it is called 
-//         (as parameters in simple functions)
-// RU:     =>  это элементы, которые передаются через
-//         компонент при его вызове
-//         (как параметры в простые функции)
-// 
-
+// hooks - useState
+// darkMode example
+// onClick={...} with try-catch 
 
 function About({ title, children }) {
+    const [theme, setTheme] = useState(false)
+
+
     return (
         <div className="about-page">
+            <h2>{theme}</h2>
+            
             <h1>{ title }</h1>
             <div>
                 { children }
