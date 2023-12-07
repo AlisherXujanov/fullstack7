0cc3f7b25c46605239dfa8b898c7461abc9dee07
Alisher <alisherxujanov163@gmail.com>
2023-12-07 08:41:39 +0500

    Routes initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import AllComponents from "./components/AllComponents"
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <AllComponents />
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/AllComponents.jsx
+++ frontend/src/components/AllComponents.jsx
@@ -1,12 +1,20 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, Route, Routes } from 'react-router-dom'
 import Navigation from "./Navigation"
 import About from "./About"
 
 
 function AllComponents(props) {
+    const location = useLocation()
     return (
         <>
-
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Navigation />} >
+                    <Route index element={<About />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
         </>
     );
 }
