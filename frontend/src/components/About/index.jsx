700cd8d33646d2045026c9c7ba6f9d029eeae1a8
Alisher <alisherxujanov163@gmail.com>
2023-12-07 09:39:28 +0500

    reset style

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,10 +1,18 @@
 import "./style.scss"
+import AboutPic from "../../assets/images/about.png"
 
 function About() {
 
     return (
-        <div className="about-wrapper">
-            <h2>About</h2>
+        <div id="about-wrapper">
+            <h2>О нас</h2>
+
+            <div className="about">
+                <div className="left">
+                    <img src={AboutPic} alt="About" width="100%" height={400} />
+                </div>
+                <div className="right"></div>
+            </div>
         </div>
     );
 }
