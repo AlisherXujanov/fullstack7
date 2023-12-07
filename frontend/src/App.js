14aeeedb91002a783eaf2e74fe8a7e4845ee1ae4
Alisher <alisherxujanov163@gmail.com>
2023-12-07 08:30:52 +0500

    AllComponents created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,9 @@
-import Navigation from "./components/Navigation"
-import About from "./components/About"
+import AllComponents from "./components/AllComponents"
 
 function App() {
-  // const x = 1+1 == 2  ? 'malenkiy' : 'NE malenkiy'
-  // let a = {
-  //     x: 1,
-  //     y: {
-  //       z: 4
-  //     }
-  // }
-  // a.y.z 
-  // a.y?.z
-  // ... || ...
-  // [] && ...
-
   return (
     <div className="App">
-      <nav>
-        <Navigation />
-      </nav>
-
-      <main>
-        <About title={"About page ..."} >
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, temporibus, recusandae veniam, doloribus minima officiis debitis nihil error est illum magni molestias vitae facilis laudantium. Incidunt saepe tenetur aliquam ullam.</p>
-        </About>
-      </main>
+      
     </div>
   );
 }
---
+++ frontend/src/components/AllComponents.jsx
@@ -0,0 +1,14 @@
+import { useLocation } from 'react-router-dom'
+import Navigation from "./Navigation"
+import About from "./About"
+
+
+function AllComponents(props) {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default AllComponents;
