f33212a555372c2d8cd2a797035bf39897c24757
Alisher <alisherxujanov163@gmail.com>
2023-12-07 09:06:45 +0500

    started clean tree

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,73 +1,10 @@
 import "./style.scss"
-import { useState } from "react"
 
-// hooks - useState
-// darkMode example
-// onClick={...} with try-catch 
-
-function About({ title, children }) {
-    const [theme, setTheme] = useState(false)
-    const [number, setNumber] = useState(0)
-
-
-    const [bg, setBg] = useState("")
-    const [color, setColor] = useState("")
-    const pStyle = {
-        backgroundColor: bg,
-        color: color
-    }
-
-    const themeStyle = {
-        backgroundColor: theme ? "#333" : "#fff",
-        color: theme ? "#fff" : "black"
-    }
-
-    function toggle(e) {
-        try {
-            setTheme(!theme)
-            // let response = "..."
-            // console.log(response.data)
-        } catch (e) {
-            console.error(e.message)
-        }
-    }
+function About() {
 
     return (
-        <div style={themeStyle} className="about-page">
-
-            <h1>{title}</h1>
-            <div>
-                {children}
-            </div>
-
-            <hr />
-            {/* <button onClick={() => {setTheme(!theme)}}>
-                Toggle theme
-            </button> */}
-            <button onClick={toggle}>
-                Toggle theme
-            </button>
-
-            <hr />
-
-            <h2>{number}</h2>
-            <button onClick={() => { setNumber(number + 1) }}>Increment +</button>
-            <button onClick={() => { setNumber(number - 1) }}>Decrement -</button>
-
-            <hr />
-
-            <p style={pStyle}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, cumque, assumenda, in aspernatur illum dolores porro itaque nemo unde adipisci voluptates aperiam facilis rem ipsa perspiciatis debitis tempora quasi accusantium!
-            </p>
-            <input 
-                type="color"
-                onChange={(e) => {setBg(e.target.value)}}
-            />
-
-            <input 
-                type="color"
-                onChange={(e) => {setColor(e.target.value)}}
-            />
+        <div className="about-wrapper">
+            <h2>About</h2>
         </div>
     );
 }
--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,27 +1,35 @@
 import './style.scss'
+import { Outlet } from 'react-router-dom'
 
 function Navigation() {
     return (
-        <div className="nav-wrapper">
-            <div className="logo">
-                <h2>Fonte</h2>
+        <div>
+            <div className="nav-wrapper">
+                <div className="logo">
+                    <h2>Fonte</h2>
+                </div>
+
+                <div className="nav-links">
+                    <a href="/#О-нас">О нас</a>
+                    <a href="/#Команда">Команда</a>
+                    <a href="/#Блог">Блог</a>
+                    <a href="/#Продукты">Продукты</a>
+                    <a href="/#Контакты">Контакты</a>
+                </div>
+
+                <div className="auth">
+                    <button className="warning-btn">
+                        Войти
+                    </button>
+                    <a href="/#Рус">Рус</a>
+                </div>
             </div>
 
-            <div className="nav-links">
-                <a href="/#О-нас">О нас</a>
-                <a href="/#Команда">Команда</a>
-                <a href="/#Блог">Блог</a>
-                <a href="/#Продукты">Продукты</a>
-                <a href="/#Контакты">Контакты</a>
-            </div>
-
-            <div className="auth">
-                <button className="warning-btn">
-                    Войти
-                </button>
-                <a href="/#Рус">Рус</a>
+            <div id='outlet'>
+                <Outlet />
             </div>
         </div>
+
     );
 }
 
