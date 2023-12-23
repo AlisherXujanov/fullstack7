f4de9d3f779918ceec1f32a5f2ceffe7499b21d9
Alisher <alisherxujanov163@gmail.com>
2023-12-23 09:45:30 +0500

    carousel implemented

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -6,11 +6,23 @@ import Values from "./Values.jsx"
 import Team from "./Team.jsx"
 import Marquee from "../common/Marquee"
 import ContactsForm from "../ContactsForm"
+import CarouselComponent from "../common/Carousel"
+
+import Img1 from "../../assets/images/img1.png"
+import Img2 from "../../assets/images/img2.png"
+import Img3 from "../../assets/images/img3.png"
+import Img4 from "../../assets/images/img4.png"
+import Img5 from "../../assets/images/img5.png"
 
 function About() {
+    const images = [Img1, Img2, Img3, Img4, Img5]
 
     return (
         <div id="about-wrapper">
+            <CarouselComponent images={images} blurred={true}>
+                {/* ... */}
+            </CarouselComponent>
+
 
             <div className="about">
                 <Heading size={2}>О нас</Heading>
--- frontend/src/components/common/Carousel/index.jsx
+++ frontend/src/components/common/Carousel/index.jsx
@@ -3,23 +3,27 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 function CarouselComponent(props) {
+    const imgStyle = {
+        filter: props.blurred ? "grayscale(100%)" : "none"
+    }
     return (
         <>
-            <Carousel>
-                <div className="carousel-c-children">
-                    { props.children }
-                </div>
+            <div className="carousel-c-children">
+                { props.children }
+            </div>
+            <Carousel autoPlay={true} infiniteLoop={true}>
                 {
                     props.images.map((img, index) => {
                         return (
                             <div key={index}>
                                 <img 
+                                    style={imgStyle}
                                     src={img} 
                                     alt={"Carousel-item-" + index} 
                                     width={"100%"}
                                     height="600"
                                 />
-                            </div>        
+                            </div>
                         )
                     })
                 }
