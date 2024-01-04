b8f427c9c91f5a8e02b45cbae5e80a70eedbdb04
Alisher <alisherxujanov163@gmail.com>
2024-01-04 10:04:18 +0500

    burger-menu

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -2,17 +2,24 @@ import './style.scss'
 import { Outlet } from 'react-router-dom'
 import Nav from "./Nav.jsx"
 import Footer from "../Footer"
+import { useState } from 'react'
 
 function Navigation() {
+    const [burgerMenuOpened, setBurgerMenuOpened] = useState(false)
+
     return (
         <div>
             <header>
-                <div className="burger-menu-wrapper">
+                <div className={burgerMenuOpened ? "burger-menu-wrapper opened" : "burger-menu-wrapper"}>
                     <div className='first-div'></div>    
                     <div className='second-div'></div>    
                     <div className='third-div'></div>    
                 </div>
-                <input id='burger-menu-toggler' type="checkbox" />
+                <input  
+                    id='burger-menu-toggler' 
+                    type="checkbox" 
+                    onClick={(e) => {setBurgerMenuOpened(e.target.checked)}}
+                />
                 {/* ...:checked + .nav-wrapper > .menu */}
                 <Nav />
             </header>
