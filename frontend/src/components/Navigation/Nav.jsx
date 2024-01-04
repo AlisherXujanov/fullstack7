b10735bd4d236e41bff79aab32014177007c1450
Alisher <alisherxujanov163@gmail.com>
2024-01-04 08:48:53 +0500

    Nav component

---
+++ frontend/src/components/Navigation/Nav.jsx
@@ -0,0 +1,46 @@
+import Heading from "../common/Heading"
+import { Link, useNavigate } from 'react-router-dom'
+
+
+function Nav(props) {
+    const navigate = useNavigate();
+
+    const goToTeamsHash = () => {
+        navigate('/about');
+        setTimeout(() => {
+            const element = document.getElementById('teams');
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }, 0);
+    };
+
+    return (
+        <div className="nav-wrapper">
+            <div className="logo">
+                <h2>
+                    <Link to={"/"}>
+                        <Heading size={1.5}>Fonte</Heading>
+                    </Link>
+                </h2>
+            </div>
+
+            <div className="nav-links">
+                <Link to={"/about"}>О нас</Link>
+                <button onClick={goToTeamsHash}>Команда</button>
+                <Link to={"/blog"}>Блог</Link>
+                <Link to={"/products"}>Продукты</Link>
+                <Link to={"/contacts"}>Контакты</Link>
+            </div>
+
+            <div className="auth">
+                <button className="warning-btn">
+                    Войти
+                </button>
+                <a href="/#Рус">Рус</a>
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
