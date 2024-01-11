d6738fd05fa3e257a2473318aef7022ff9b20ae5
Alisher <alisherxujanov163@gmail.com>
2024-01-11 09:29:37 +0500

    del unnecessary elements

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -9,18 +9,6 @@ function Products(props) {
 
     function dec(e) { state.dispatch({type: "decrement"}) }
 
-    const names = ['John', 'Bob', 'Alice', 'Kate']
-    const colors = ['red', 'green', 'blue', 'yellow']
-    const randomNumber = Math.floor(Math.random() * names.length)
-
-    function setColor(e) { state.dispatch({type: 'color', color: colors[randomNumber]}) }
-    function setName(e)  { state.dispatch({type: 'name',  name: names[randomNumber]}) }
-
-    function setLeft(e)  { state.dispatch({type: "left"}) }
-    function setMiddle(e) { state.dispatch({type: "middle"}) }
-    function setRight(e) { state.dispatch({type: "right"}) }
-    
-
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
@@ -37,19 +25,6 @@ function Products(props) {
                 <br />
                 <button className='warning-btn' onClick={dec}>Decrement</button>
                 <button className='warning-btn' onClick={inc}>Increment</button>
-                <br />
-                <br />
-                <hr />
-                <br />
-                <button className='warning-btn' onClick={setColor}>Color</button>
-                <button className='warning-btn' onClick={setName}>Name</button>
-                <br />
-                <br />
-                <hr />
-                <br />
-                <button className='warning-btn' onClick={setLeft}>Left</button>
-                <button className='warning-btn' onClick={setMiddle}>Middle</button>
-                <button className='warning-btn' onClick={setRight}>Right</button>
             </div>
         </div>
     );
