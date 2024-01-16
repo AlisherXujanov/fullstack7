01d50163b425e83d7268c3f90bd151c0def1e08f
Alisher <alisherxujanov163@gmail.com>
2024-01-16 08:39:42 +0500

    del all unnecery items

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -1,46 +1,32 @@
 import './style.scss'
-import { useContext, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import { globalContext } from '../../state'
-import Test from "./Test.jsx"
 
 function Products(props) {
-    // const state = useContext(globalContext)
-    // function inc(e) { state.dispatch({type: "increment"}) }
-    // function dec(e) { state.dispatch({type: "decrement"}) }
-    const [count, setCount] = useState(0)
+    const state = useContext(globalContext)
+    
+    function inc(e) { state.dispatch({type: "increment"}) }
+
+    function dec(e) { state.dispatch({type: "decrement"}) }
+
+
 
     return (
         <div id="products-wrapper">
             <h2>Products</h2>
             <div>
-                {/* <p style={{
+                <p style={{
                     color: state.color, 
                     textAlign: 'center', 
                     transform: state.transform
                 }}>
                     { state.count } by { state.name }
-                </p> */}
-
-                <p>{count}</p>
+                </p>
 
                 <br />
                 <br />
-                <button 
-                    className='warning-btn' 
-                    onClick={() => {setCount(count - 1)}}
-                >
-                    Decrement
-                </button>
-                <button 
-                    className='warning-btn' 
-                    onClick={() => {setCount(count + 1)}}
-                >
-                    Increment
-                </button>
-
-                <hr />
-                <hr />
-                <Test />
+                <button className='warning-btn' onClick={dec}>Decrement</button>
+                <button className='warning-btn' onClick={inc}>Increment</button>
             </div>
         </div>
     );
