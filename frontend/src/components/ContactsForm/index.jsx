5ff916c11e28c9f7734a968bb49cdf5b3552b2e8
Alisher <alisherxujanov163@gmail.com>
2024-01-18 09:21:01 +0500

    inputs

--- frontend/src/components/ContactsForm/index.jsx
+++ frontend/src/components/ContactsForm/index.jsx
@@ -22,15 +22,21 @@ function ContactsForm() {
 
     return (
         <div id="contacts-form-wrapper">
-            <h2>{form.email}</h2>
-
             <form onSubmit={submit}>
-                <input id="full-name-input" type="text" placeholder="Полное имя" name='name' /> 
+                <input 
+                    id="full-name-input" type="text" 
+                    placeholder="Полное имя" name='name' 
+                    onChange={fireSetForm}
+                /> 
                 <div>
-                    <input id="email-input" type="email" placeholder="Почта" 
+                    <input 
+                        id="email-input" type="email" placeholder="Почта" 
                         onChange={fireSetForm} name='email'
                     />
-                    <input id="number-input" type="number" placeholder="Номер телефона" name='number' />
+                    <input 
+                        id="number-input" type="number" placeholder="Номер телефона" 
+                        onChange={fireSetForm} name='number'
+                    />
                 </div>
                 <textarea 
                     id="description-area" 
@@ -38,6 +44,7 @@ function ContactsForm() {
                     rows="8"
                     placeholder="Текст сообщения"
                     name='notes'
+                    onChange={fireSetForm}
                 ></textarea>
                 <button className="warning-btn">
                     Получить консультацию
