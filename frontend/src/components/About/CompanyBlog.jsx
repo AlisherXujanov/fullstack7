cfb2ee938b5228b6568c220c6a41afd85ff9023e
Alisher <alisherxujanov163@gmail.com>
2024-01-20 09:19:13 +0500

    created a logic for gethering slide info

--- frontend/src/components/About/CompanyBlog.jsx
+++ frontend/src/components/About/CompanyBlog.jsx
@@ -1,6 +1,23 @@
 import CarouselComponent from "../common/Carousel"
+import blogs from "../../db/blog.json"
 
-const slides = []
+
+// Separate blog items by 3 items per slide
+let slides = []
+for (let item of blogs) {
+    let last_arr_slide = slides[slides.length - 1]
+
+    if (!last_arr_slide) {
+        slides.push([item])
+    } else {
+        if (last_arr_slide.length < 3) {
+            last_arr_slide.push(item)
+        } else {
+            slides.push([item])
+        }
+    }
+}
+
 
 function CompanyBlog() {
     return (
