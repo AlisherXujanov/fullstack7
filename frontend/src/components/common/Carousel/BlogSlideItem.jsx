040e99badb644ccc33b701f2b5bc8af27753a4e3
Alisher <alisherxujanov163@gmail.com>
2024-01-23 08:42:02 +0500

    images

--- frontend/src/components/common/Carousel/BlogSlideItem.jsx
+++ frontend/src/components/common/Carousel/BlogSlideItem.jsx
@@ -2,22 +2,24 @@ import "./slideStyle.scss"
 import Blog1 from "../../../assets/images/blog-1.png"
 import Blog2 from "../../../assets/images/blog-2.png"
 import Blog3 from "../../../assets/images/blog-3.png"
+import Blog4 from "../../../assets/images/img2.png"
+import Blog5 from "../../../assets/images/img3.png"
+
 
 
 function BlogSlideItem(props) {
-    // "id"   // "title"   // "author"   // "date"   // "subtitle1"   // "subtitle2"
-    const images = [Blog1, Blog2, Blog3]
+    const images = [Blog1, Blog2, Blog3, Blog4, Blog5]
 
     let img_index = null
-    
-    if (props.info.id < 3) { img_index = props.info.id } 
 
-    else { img_index = props.info.id % 3 }
+    if (props.info.id < images.length) { img_index = props.info.id } 
+
+    else { img_index = props.info.id % images.length }
 
     return (
         <div className="slide-content">
             <img src={images[img_index]} alt="" />
-            <h2>{props.info.title}</h2>
+            <h3>{props.info.title}</h3>
             <button className="warning-btn">
                 Подробнее
             </button>
