b9b0d3132a465a4f3a5ccce6097e88770b4a8924
AlisherKhujanov <alisherxujanov163@gmail.com>
2024-05-30 09:04:57 +0500

    CRUD

--- backend/app/posts/urls.py
+++ backend/app/posts/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('create-post', create_post, name='create-post'),
+    path('update-post/<int:pk>', update_post, name='update-post'),
+    path('view-post/<int:pk>', view_post, name='view-post'),
+    path('delete-post/<int:pk>', delete_post, name='delete-post'),
     path('posts-list', posts_list, name='posts-list'),
 ]
--- backend/app/posts/views.py
+++ backend/app/posts/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Posts
 from .forms import PostsForm
 
+# import messages and use it for all views to inform the user about the success of the operation
+# RU: импортируем messages и используем его для всех представлений, чтобы информировать пользователя о успешности операции
+from django.contrib import messages
+
+# CRM - it means Create, Read, Update, Delete
+# By convention, the order of the functions is:
+# 1. C  -  Create
+# 2. R  -  Read
+# 3. M  -  Modify (Update)  / Delete
+
 
 def posts_list(request):
     posts = Posts.objects.all()
@@ -13,9 +23,37 @@ def create_post(request):
         form = PostsForm(request.POST)
         if form.is_valid():
             form.save()
+        messages.success(request, 'Post created successfully!')
         return redirect('posts-list')
 
-    else: # get method
+    else:  # get method
         form = PostsForm()
 
     return render(request, 'create_post.html', context={'form': form})
+
+
+# pk  =>  primary key
+def update_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    form = PostsForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostsForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.warning(request, 'Post updated successfully!')
+            return redirect('posts-list')
+
+    return render(request, 'update_post.html', context={'form': form})
+
+
+def view_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    return render(request, 'view_post.html', context={'post': post})
+
+
+def delete_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    post.delete()
+    messages.success(request, 'Post deleted successfully!')
+    return redirect('posts-list')
