6902eeb36757477f483dd37d816671e147949957
AlisherKhujanov <alisherxujanov163@gmail.com>
2024-06-02 22:58:45 +0500

    updates

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -3,24 +3,31 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 
+CHOICES = (
+    ('1', "Male"),
+    ('2', 'Female'),
+    ('3', 'Other'),
+)
+
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age = models.IntegerField(null=True, blank=True)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
     bio = models.TextField(null=True, blank=True)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    gender = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    age = models.IntegerField(default=0)
+    gender = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile'
-    
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-    
-        img = Image.open(self.image.path)
+
+        img = Image.open(self.image.path).convert('RGB')
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-
-            
--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -5,40 +5,36 @@ from .models import Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-# Create your views here.
 def create_user(request):
     form = UserForm()
 
     if request.method == 'POST':
         form = UserForm(request.POST, request.FILES)
+        
         if form.is_valid():
             username = form.cleaned_data['username']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
             email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            password2 = form.cleaned_data['password2']
 
-            age = form.cleaned_data['age']
-            # gender = form.cleaned_data['choice']
-            image = form.cleaned_data['image']
+            if password != password2:
+                # OR USE is_valid() METHOD from inside the form
+                messages.error(request, 'Passwords do not match')
+                return redirect('create_user')
+            if user := User.objects.filter(username=username).first():
+                messages.error(request, 'Username already exists')
+                return redirect('create_user')
 
-            user = User.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-            )
+            user = User.objects.create_user(username=username, password=password)
             user.save()
 
-            profile = Profile.objects.create(
-                user=user,
-                age=age,
-                # gender=gender,
-                image=image,
-                bio='',
-            )
+            profile = Profile.objects.create(user=user)
             profile.save()
 
-            messages.success(request, f'Welcome {first_name} to our platform!')
+            messages.success(request, f'Welcome {username} to our platform!')
             return redirect('home')
+        else:
+            print("FORM IS NOT VALID")
+            print(form.errors)
 
     return render(request, 'create_user.html', {'form': form})
