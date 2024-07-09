6e3f3c06b03bbb6f5e46e589fcdb041ac4857430
Alisher <alisherxujanov163@gmail.com>
2024-07-09 09:00:40 +0500

    serializers

--- backend/app/posts/serializers.py
+++ backend/app/posts/serializers.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
+from .models import Posts
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ['url', 'title', 'content', 'date_posted']
 
 
